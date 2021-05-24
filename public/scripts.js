ed295c3e4e2ad969a781305bbfdb48eb16e7f7bb
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2021-05-24 11:26:38 +0530

    Adding user to a namespace completed and some refactoring

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,22 @@
-const socket = io("http://localhost:9000");
-socket.on("messageFromServer", (dataFromServer) => {
-  console.log(dataFromServer);
-  socket.emit("messageToServer", { data: "Data from the Client!" });
-});
-document.querySelector("#message-form").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const newMessage = document.querySelector("#user-message").value;
-  socket.emit("newMessageToServer", { text: newMessage });
-});
+const socket = io("http://localhost:9000"); // Entry to main (/) namespace
 
-socket.on("messageToCliets", (msg) => {
-  document.querySelector("#messages").innerHTML += `<li>${msg.text}</li>`;
+// Listen for nsList which is a list of all the namespaces
+socket.on("nsList", (nsData) => {
+  console.log("The list of namespace has arrived");
+
+  let namespacesDiv = document.querySelector(".namespaces");
+  namespacesDiv.innerHTML = "";
+  nsData.forEach((ns) => {
+    namespacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.img}" /></div>`;
+  });
+
+  // Add a click listener for each namespace
+  Array.from(document.getElementsByClassName("namespace")).forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      const nsEndpoint = elem.getAttribute("ns");
+      console.log(nsEndpoint);
+    });
+  });
+
+  joinNS("/wiki");
 });
--- slack.js
+++ slack.js
@@ -2,31 +2,37 @@ const express = require("express");
 const app = express();
 const socketio = require("socket.io");
 
+let namespaces = require("./data/namespaces");
 app.use(express.static(__dirname + "/public"));
-
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
 // connect or connection events are predefined in socket io which is fired upon a connection from the client
 // socket parameter is the underline socket or the client that has just connected
-io.on("connection", (socket) => {
-  socket.emit("messageFromServer", { data: "Welcome to the socket io server" });
-  socket.on("messageToServer", (dataFromClient) => {
-    console.log(dataFromClient);
-  });
-  socket.on("newMessageToServer", (msg) => {
-    io.emit("messageToCliets", { text: msg.text });
-  });
 
-  // The server can still communicate across namespaces
-  // but on the clientInformation, the socket needs to be in that namespace
-  // in order to get the events
-  setTimeout(() => {
-    io.of("/admin").emit("Wekcome to the admin channel, from the main channel");
-  }, 2000);
+// Here as soon as somebody connects, build the array of namespaces(img, endpoint) and send it to that client (socket)
+io.on("connection", (socket) => {
+  // Build and array to send back with the image and endpoint for each namespace
+  let nsData = namespaces.map((ns) => {
+    return {
+      img: ns.img,
+      endpoint: ns.endpoint,
+    };
+  });
+  // Send nsData back to the client
+  // We need to use socket, NOT io, because we want it to go to just this client
+  socket.emit("nsList", nsData);
+  // io.emit("nsList", nsData); This would send it to every body connected to the main namespace
+  // As soon as somebody joined the server, every body would get the list which we don't want
 });
 
-io.of("/admin").on("connection", (socket) => {
-  console.log("Someone connected to the admin channel");
-  io.of("/admin").emit("welcome", "Welcome to the admin channel");
+// Loop through each namespace and listen for a connection
+namespaces.forEach((namespace) => {
+  // console.log(namespace);
+  io.of(namespace.endpoint).on("connection", (nsSocket) => {
+    console.log(`${nsSocket.id} has joined ${namespace.endpoint}`);
+    // A socket has connected to one of our chat group namespaces
+    // Send that ns group info back
+    nsSocket.emit("nsRoomLoad", namespaces[0].rooms);
+  });
 });
